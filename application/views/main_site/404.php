ee3166229187da2b537c5ab94b34e88effdeaee9
Zobaer Mobin <31048775+23mobin@users.noreply.github.com>
2017-09-25 15:43:33 +0600

    v

--- application/views/main_site/404.php
+++ application/views/main_site/404.php
@@ -1,9 +1,5 @@
-<!-- single page wrapper ends-->
 <div class="wthree text-center">
-  <!-- directed blogs -->
-
   <div class="tech-btm">
-
     <h2 class="wow fadeInDown" data-wow-duration=".8s" data-wow-delay=".2s">ERROR</h2>
     <h4><div class="font404 wow fadeInLeft" data-wow-duration=".5s" data-wow-delay="0.2s">404</div></h4>
     <h2 class="wow fadeInUp" data-wow-duration=".8s" data-wow-delay=".2s">Content not found</h2>
