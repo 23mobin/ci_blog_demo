b442d2148ed7711de9693f5f4c8baaaff4c4c81e
Zobaer Mobin <31048775+23mobin@users.noreply.github.com>
2017-09-26 02:28:47 +0600

    user_home controller added

---
+++ application/controllers/user_home.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+session_start();
+class User_home extends CI_Controller {
+  public function __construct(){
+    parent::__construct();
+    $u_id = $this->session->userdata('u_id');
+    if ($u_id == NULL) {
+      redirect(base_url(),'refresh');
+    }
+  }
+
+	public function index()
+	{
+    $data = array();
+    $data['u_name']= $this->session->userdata('u_name');
+    $data['u_id']= $this->session->userdata('u_id');
+    $data['u_home_content']=$this->load->view('user/user_home_content',$data,true);
+    $data['u_navbar']=$this->load->view('user/user_navbar_v',$data,true);
+		$this->load->view('user/user_home_v',$data);
+  }
+  public function page1()
+	{
+    $data = array();
+    $data['u_name']= $this->session->userdata('u_name');
+    $data['u_id']= $this->session->userdata('u_id');
+    $data['u_home_content']=$this->load->view('user/user_page1',$data,true);
+    $data['u_navbar']=$this->load->view('user/user_navbar_v',$data,true);
+		$this->load->view('user/user_home_v',$data);
+}
+
+  public function logout()
+	{
+    $this->session->unset_userdata('u_id');
+    $this->session->unset_userdata('u_name');
+		redirect(base_url(),'refresh');
+  }
+}
