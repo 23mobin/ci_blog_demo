1dab2282064dce520d88c64a9560c5651585347e
Zobaer Mobin <31048775+23mobin@users.noreply.github.com>
2017-09-25 15:47:36 +0600

    404 saved

--- application/views/main_site/404.php
+++ application/views/main_site/404.php
@@ -4,4 +4,5 @@
     <h4><div class="font404 wow fadeInLeft" data-wow-duration=".5s" data-wow-delay="0.2s">404</div></h4>
     <h2 class="wow fadeInUp" data-wow-duration=".8s" data-wow-delay=".2s">Content not found</h2>
   </div>
+
 </div>
